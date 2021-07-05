36b77eca3828c05120be2f11860d74fff7789dd8
Tim Schraml <schramltim@gmail.com>
2021-07-05 22:55:36 +0200

    add callable endpoints

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,7 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Interfaces\RouteCollectorProxyInterface;
 use Taberu\Middleware\Cors;
 use Taberu\Middleware\JsonTokenAuthentication;
 use Taberu\Controller\DefaultController;
@@ -24,10 +25,37 @@ $app->get('[/]', function (Request $request, Response $response, array $args) {
     return $response;
 });
 
-$app->get('/clothing[/]', DefaultController::class.':addEntry')->add(new JsonTokenAuthentication());
 
-$app->post('/user/register[/]', UserController::class.':register');
-$app->post('/user/login[/]', UserController::class.':login');
-$app->post('/user/logout[/]', UserController::class.':logout');
+
+$app->group('/v1', function (RouteCollectorProxyInterface $group) use ($app) {
+    $app->group('/users', function (RouteCollectorProxyInterface $group) use ($app) {
+        $app->post('/register[/]', UserController::class.':register');
+        $app->post('/login[/]', UserController::class.':login');
+        $app->post('/logout[/]', UserController::class.':logout')->add(new JsonTokenAuthentication());
+
+        $app->get('/{userId}[/]', UserController::class.':getSpecificUser')->add(new JsonTokenAuthentication());
+        $app->patch('/{userId}[/]', UserController::class.':updateUser')->add(new JsonTokenAuthentication());
+        $app->get('[/]', UserController::class.':getAllUsers')->add(new JsonTokenAuthentication());
+        $app->get('/{userId}/orders[/]', UserController::class.':getUserOrders')->add(new JsonTokenAuthentication());
+        $app->get('/{userId}/menus[/]', UserController::class.':getUserMenus')->add(new JsonTokenAuthentication());
+    });
+
+    $app->group('/departments', function (RouteCollectorProxyInterface $group) use ($app) {
+        $app->get('[/]', DeparmentController::class.':getAllDepartments');
+        $app->post('[/]', DeparmentController::class.':createDepartment');
+        $app->get('/{departmentId}[/]', DeparmentController::class.':getSpecificDepartment');
+        $app->patch('/{departmentId}[/]', DeparmentController::class.':updateDepartment');
+        $app->delete('/{departmentId}[/]', DeparmentController::class.':deleteDepartment');
+        $app->delete('/{departmentId}/users[/]', DeparmentController::class.':getAllDepartmentUser');
+    })->add(new JsonTokenAuthentication());
+
+    $app->group('/orders', function (RouteCollectorProxyInterface $group) use ($app) {
+        $app->get('/{orderId}[/]', DeparmentController::class.':getSpecificOrder');
+    })->add(new JsonTokenAuthentication());
+});
+
+
+
+
 
 $app->run();
