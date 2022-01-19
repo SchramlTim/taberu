f7044dfe64ec607082903b32d35f3c71cd7b392a
tim.schraml <tim@bergfreunde.de>
2022-01-19 21:27:51 +0100

    improve burge menu styling

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -8,16 +8,19 @@ function BowlDetails() {
       }, [display])
     return (
         <>
-            <div className={'bg-amber-600 h-16'}>
-                <div className={'absolute right-2 z-50 bg-white h-12 w-12'} onClick={toggleMenu}>Öffnen</div>
+            <div className={'bg-amber-600 h-16 flex items-center'}>
+                <button className={'absolute right-2 z-50 bg-white h-12 w-20 rounded-2xl flex justify-center items-center'} onClick={toggleMenu}>
+                    <div>Öffnen</div>
+                </button>
                 {display &&
-                <div className={'absolute right-0 bg-red-700 h-screen top-0 m-0 w-1/4'}>
-                <ul >
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Register</Link></li>
-                    <li><Link to="/user">Users</Link></li>
-                    <li><Link to="/bowls">Bowls</Link></li>
-                </ul></div>}
+                <div className={'absolute right-0 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 bg-gray-800 border border-gray-200 h-screen top-0 m-0 w-1/4'}>
+                    <ul className={'mt-20 flex flex-col justify-start text-center'}>
+                        <li className={'mt-2'}><Link to="/login">Login</Link></li>
+                        <li className={'mt-2'}><Link to="/register">Register</Link></li>
+                        <li className={'mt-2'}><Link to="/user">Users</Link></li>
+                        <li className={'mt-2'}><Link to="/bowls">Bowls</Link></li>
+                    </ul>
+                </div>}
             </div>
         </>
       );
--- frontend/src/Layout/DefaultLayout/DefaultLayout.tsx
+++ frontend/src/Layout/DefaultLayout/DefaultLayout.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Header from '../../Components/Header/Header';
 
 const DefaultLayout = ({children}: any) => (
-  <>
+  <div className={'font-quicksand'}>
     <Header />
     <div className={'mt-10'}>
       {children}
     </div>
-  </>
+  </div>
 );
 
 export {DefaultLayout};
