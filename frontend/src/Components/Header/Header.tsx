f9180fc02a6a71ac434f1ec8df21ca8010c7a1fd
tim.schraml <tim@bergfreunde.de>
2022-01-19 21:44:43 +0100

    improve burge menu styling

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -8,19 +8,28 @@ function BowlDetails() {
       }, [display])
     return (
         <>
-            <div className={'bg-amber-600 h-16 flex items-center'}>
-                <button className={'absolute right-2 z-50 bg-white h-12 w-20 rounded-2xl flex justify-center items-center'} onClick={toggleMenu}>
-                    <div>Öffnen</div>
+            <div className={'h-16 flex items-center bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 bg-gray-800 border border-gray-200'}>
+                <Link to="/home" className={'text-xl'}>Taberu</Link>
+
+                <button className={'absolute right-2 z-50 bg-white h-12 w-12 rounded-2xl flex justify-center items-center'} onClick={toggleMenu}>
+                    <div className="space-y-2">
+                        <div className="w-8 h-0.5 bg-gray-600"></div>
+                        <div className="w-8 h-0.5 bg-gray-600"></div>
+                        <div className="w-8 h-0.5 bg-gray-600"></div>
+                    </div>
                 </button>
                 {display &&
-                <div className={'absolute right-0 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 bg-gray-800 border border-gray-200 h-screen top-0 m-0 w-1/4'}>
-                    <ul className={'mt-20 flex flex-col justify-start text-center'}>
-                        <li className={'mt-2'}><Link to="/login">Login</Link></li>
-                        <li className={'mt-2'}><Link to="/register">Register</Link></li>
-                        <li className={'mt-2'}><Link to="/user">Users</Link></li>
-                        <li className={'mt-2'}><Link to="/bowls">Bowls</Link></li>
-                    </ul>
-                </div>}
+                <>
+                    <div onClick={toggleMenu} className={'absolute top-0 left-0 w-screen h-screen'}></div>
+                    <div className={'absolute right-0 bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-90 bg-gray-800 border h-screen border-gray-200 top-0 m-0 w-1/4 text-white'}>
+                        <ul className={'mt-20 flex flex-col justify-start text-center text-2xl'}>
+                            <li className={'mt-2'}><Link to="/login">Login</Link></li>
+                            <li className={'mt-2'}><Link to="/register">Register</Link></li>
+                            <li className={'mt-2'}><Link to="/user">Users</Link></li>
+                            <li className={'mt-2'}><Link to="/bowls">Bowls</Link></li>
+                        </ul>
+                    </div>
+                </>}
             </div>
         </>
       );
