9b8ee5b89522e2fc53815347400a25db00d2c69a
tim.schraml <tim@bergfreunde.de>
2022-05-02 22:26:07 +0200

    fix layout issue in user list template

--- frontend/src/Routes/User/User.tsx
+++ frontend/src/Routes/User/User.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {get} from "../../Utils/Request";
 
 function User() {
 
@@ -6,36 +7,18 @@ function User() {
 
     // Similar to componentDidMount and componentDidUpdate:
     useEffect(() => {
-        getData("https://taberu.localhost/v1/users/")
+        get("/v1/users/")
             .then(response => {
                 setUser(response.data)
             });
     }, []);
-
-    const getData = async (url = '', data = {}) => {
-        // Default options are marked with *
-        const response = await fetch(url, {
-            method: 'GET', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'x-token': sessionStorage.getItem('token') ?? ''
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        });
-        return response.json(); // parses JSON response into native JavaScript objects
-    }
 
       return (
           <ul className={"bg-light"}>
               {
                   user.map((userObject: any) => (
                   <li key={userObject.id}>
-                      {JSON.stringify(userObject)}
+                      <span>{userObject.username}</span>
                   </li>
               ))}
           </ul>
