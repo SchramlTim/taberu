59e9453d87dcb341d10862b96c02703e44dc23dd
Tim Schraml <tim@bergfreunde.de>
2023-05-18 22:36:41 +0200

    adjust color schema

--- frontend/src/Components/TextInput/TextInput.tsx
+++ frontend/src/Components/TextInput/TextInput.tsx
@@ -9,12 +9,12 @@ function TextInput(props: {
 }) {
     return (
         <div className={"mb-4"}>
-            <label className={"block text-gray-700 text-sm font-bold mb-2"} htmlFor={props.id}>
+            <label className={"block text-text-primary text-sm font-bold mb-2"} htmlFor={props.id}>
                 {props.title}
             </label>
             <input
                 onChange={props.onChange}
-                className={"shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"}
+                className={"shadow appearance-none border rounded w-full py-2 px-3 text-text-primary leading-tight focus:outline-none focus:shadow-outline"}
                 id={props.id} type={props.type} placeholder={props.placeholder}/>
         </div>
     );
