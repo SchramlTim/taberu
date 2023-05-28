c7a4bef3b049687f5bf8bf483d0d160fa6dbd156
Tim Schraml <tim@bergfreunde.de>
2023-05-28 22:38:41 +0200

    improve loading animation for bowl list with stuck components

--- frontend/src/Routes/Bowls/BowlListItem/BowlListItem.tsx
+++ frontend/src/Routes/Bowls/BowlListItem/BowlListItem.tsx
@@ -29,21 +29,58 @@ function BowlListItem(bowl: BowlProps) {
                 'w-3/4 min-h-10 p-3 rounded-lg shadow-xl' +
                 ' transition ease-in-out delay-50 duration-200 hover:bg-amber-200'}
         >
-            <span className={'text-2xl w-full'}>{bowl.name} {orders.length ? `(${orders.length})` : <div className={'inline ml-1'}><Spinner/></div>}</span>
+            <span className={'text-2xl w-full'}>{bowl.name} {orders.length ? `(${orders.length})` : <LoadingOrderCount/>}</span>
             <span className={'w-full text-gray-500'}>{bowl.description}</span>
             <div className={'flex h-8 items-center'}>
-                {categories.length ? categories.map((category: any) => <div className={'flex flex-col w-8'}>
-                    <img
-                        src={category.iconUrl}
-                    />
-                </div>) : <Spinner/>}
+                {categories.length ? categories.map((category: any) => {
+                    return (
+                        <div className={'flex flex-col w-8'}><img src={category.iconUrl}/></div>
+                    )
+                }) : <LoadingCategories/>}
             </div>
             <div className={'w-full flex justify-between mt-5 text-gray-500'}>
                 <span>{(new Date(bowl.orderDeadline)).toLocaleString()}</span>
                 <span>{(new Date(bowl.arriveDate)).toLocaleString()}</span>
             </div>
         </Link>
-      );
-  }
+    );
+}
+
+export function LoadingBowlListItem() {
+    return (
+        <div
+            className={'' +
+                'flex flex-wrap gap-[0.325rem] ' +
+                'w-3/4 min-h-10 p-3 rounded-lg shadow-xl' +
+                ' transition ease-in-out delay-50 duration-200 hover:bg-amber-200'}
+        >
+            <span className={'text-2xl w-full flex animate-pulse'}><span className={'bg-gray-300 w-1/2 h-8 rounded-lg'}></span> <LoadingOrderCount/></span>
+            <span className={'flex flex-[0_0_100%] animate-pulse w-full bg-gray-300 w-10 h-6 rounded-lg'}></span>
+            <div className={'flex h-8 items-center'}>
+                <LoadingCategories/>
+            </div>
+            <div className={'w-full flex justify-between mt-5 text-gray-500'}>
+                <span className={'bg-gray-300 w-1/3 h-5 rounded-lg animate-pulse'}></span>
+                <span className={'bg-gray-300 w-1/3 h-5 rounded-lg animate-pulse'}></span>
+            </div>
+        </div>
+    )
+}
+
+function LoadingOrderCount() {
+    return (
+        <span className={'bg-gray-300 w-7 h-7 animate-pulse ml-1 rounded-full'}></span>
+    );
+}
+
+function LoadingCategories() {
+    return (
+        <div className={'flex gap-1 animate-pulse'}>
+            <div className={'h-8 w-8 bg-gray-300 rounded'}></div>
+            <div className={'h-8 w-8 bg-gray-300 rounded'}></div>
+            <div className={'h-8 w-8 bg-gray-300 rounded'}></div>
+        </div>
+    );
+}
 
 export default BowlListItem;
--- frontend/src/Routes/Bowls/Bowls.tsx
+++ frontend/src/Routes/Bowls/Bowls.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect, Suspense} from 'react';
 import { Link } from 'react-router-dom';
 import {get} from "../../Utils/Request";
 import {BowlProps} from "../../Utils/Types";
-import BowlListItem from "./BowlListItem/BowlListItem";
+import BowlListItem, {LoadingBowlListItem} from "./BowlListItem/BowlListItem";
 
 export const Bowls = () => {
 
@@ -20,9 +20,13 @@ export const Bowls = () => {
           <>
               <div className={"flex flex-col gap-y-3 justify-center items-center"}>
                   {
-                      bowls.map((bowl: BowlProps) => (
-                        <BowlListItem key={bowl.id} {...bowl} />
-                  ))}
+                      bowls.length ? bowls.map((bowl: BowlProps) => (
+                          <BowlListItem key={bowl.id} {...bowl} />
+                  )) : <>
+                        <LoadingBowlListItem/>
+                        <LoadingBowlListItem/>
+                        <LoadingBowlListItem/>
+                  </>}
               </div>
               <Link to="/bowls/create" className="bg-button-primary fixed bottom-10 right-10 text-black text-center py-2 px-4 rounded h-14 w-14 inline-flex items-center">
                   <svg className="h-8 w-8 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
