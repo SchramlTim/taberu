653bdc44d579889732e9fe0c1412e96a635f843a
Tim Schraml <tim@bergfreunde.de>
2023-06-18 22:07:20 +0200

    frist try to use goo effect for amount handler

--- frontend/src/Routes/BowlDetails/OrderableItemList/Item/AmountHandler/AmountHandler.tsx
+++ frontend/src/Routes/BowlDetails/OrderableItemList/Item/AmountHandler/AmountHandler.tsx
@@ -10,10 +10,26 @@ function AmountHandler(props: {item: MenuItemProps}) {
 
     const selfSelectList = basketItems.filter((listItem) => listItem.id === item.id)
     return (
-            <div className={'flex items-center justify-center font-bold gap-1 mr-2'}>
-                {active && <div className="h-8 w-12 bg-background-secondary animate-fadeIn flex items-center justify-center rounded-xl" onClick={() => reduceItem(item)}>-</div>}
-                <span className={'flex items-center justify-center w-12 h-12 bg-background-secondary rounded-full text-center'} onClick={() => toggle(!active)}>{selfSelectList.length}</span>
-                {active && <div className="h-8 w-12 bg-background-secondary animate-fadeIn flex items-center justify-center rounded-xl" onClick={() => increaseItem(item)}>+</div>}
+            <div className={'flex items-center justify-center font-bold gap-1 mr-2 [filter:url(#goo)]'}>
+                <div className={"h-8 w-10 bg-background-secondary flex items-center justify-center rounded-xl z-0 transition-transform duration-300 translate-x-[110%] " + (active ? "translate-x-0" : '')} onClick={() => reduceItem(item)}>-</div>
+                <span className={'flex items-center justify-center w-12 h-12 bg-background-secondary rounded-full text-center z-10'} onClick={() => toggle(!active)}>{selfSelectList.length}</span>
+                <div className={"h-8 w-10 bg-background-secondary flex items-center justify-center rounded-xl z-0 transition-transform duration-300 translate-x-[-110%] " + (active ? "translate-x-0" : '')} onClick={() => increaseItem(item)}>+</div>
+                <svg width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
+                    <defs>
+                        <filter id="goo">
+
+                            <feGaussianBlur in="SourceGraphic" stdDeviation="7" result="blur" />
+
+                            <feColorMatrix in="blur" mode="matrix" values="1  0  0  0  0
+                                    0  1  0  0  0
+                                    0  0  1  0  0
+                                    0  0  0  25  -12" result="goo" />
+
+                            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+
+                        </filter>
+                    </defs>
+                </svg>
             </div>
     )
 }
