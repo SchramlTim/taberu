32a5c535f286f5dc5a06473e823f96112e9beef7
Tim Schraml <tim@bergfreunde.de>
2023-06-19 16:59:40 +0200

    use debounce to close amount handler

--- frontend/src/Routes/BowlDetails/OrderableItemList/Item/AmountHandler/AmountHandler.tsx
+++ frontend/src/Routes/BowlDetails/OrderableItemList/Item/AmountHandler/AmountHandler.tsx
@@ -1,20 +1,27 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { BasketContext } from "../../../../../Context/BasketContext";
 import { MenuItemProps } from "../../../../../Utils/Types";
 
 function AmountHandler(props: {item: MenuItemProps}) {
 
     const [active, toggle] = useState(false)
+    const [interactionCount, setInteractionCount] = useState(0)
     const { basketItems, increaseItem, reduceItem } = useContext(BasketContext);
     const item = props.item
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+          toggle(false);
+        }, 1500);
+        return () => clearTimeout(timeoutId);
+    }, [active, interactionCount])
 
     const selfSelectList = basketItems.filter((listItem) => listItem.id === item.id)
     return (
             <div className={(active ? 'translate-x-0' : 'translate-x-[30%]') + ' flex transition-all items-center justify-end font-bold gap-1 mr-1 flex-end [filter:url(#goo)]'}>
                 <div className={"h-8 w-10 bg-background-secondary flex items-center justify-center rounded-xl z-0 transition-transform duration-300 translate-x-[110%] " + (active ? "!translate-x-0" : '')} onClick={() => {
-                
                     reduceItem(item)
-                    
+                    setInteractionCount(interactionCount+1) 
                     if (selfSelectList.length <= 1) {
                         toggle(false)
                     }
@@ -26,7 +33,10 @@ function AmountHandler(props: {item: MenuItemProps}) {
                     }
                     toggle(!active)
                 }}>{selfSelectList.length > 0 ? selfSelectList.length : '+'}</span>
-                <div className={"h-8 w-10 bg-background-secondary flex items-center justify-center rounded-xl z-0 transition-transform duration-300 translate-x-[-110%] " + (active ? "!translate-x-0" : '')} onClick={() => increaseItem(item)}>+</div>
+                <div className={"h-8 w-10 bg-background-secondary flex items-center justify-center rounded-xl z-0 transition-transform duration-300 translate-x-[-110%] " + (active ? "!translate-x-0" : '')} onClick={() => {
+                    increaseItem(item)
+                    setInteractionCount(interactionCount+1) 
+                }}>+</div>
                 <svg width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
                     <defs>
                         <filter id="goo">
